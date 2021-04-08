d2786ff237afd7d3df72b090dca50f3d7f7597b6
Liu Chuanyong <liuchuanyong@LiudeMac-mini.local>
2021-04-08 17:05:50 +0800

    权限判断

--- WOSoftLife/WOSoftLife/Expand/Utils/LSPUtils.swift
+++ WOSoftLife/WOSoftLife/Expand/Utils/LSPUtils.swift
@@ -22,13 +22,13 @@ import CoreMotion
 class LSPUtils: NSObject {
 
     // MARK: 文本计算方法
-     /// 计算文字大小
-     /// - Parameters:
-     ///   - font: 字体
-     ///   - size: 大小
-     ///   - mode: 文字展示类型
-     /// - Returns: 文字大小
-     public func calculateSizeForText(textString text: String, systemFont font: UIFont, constrainedToSize size: CGSize, lineBreakMode mode: NSLineBreakMode) -> CGSize {
+    /// 计算文字大小
+    /// - Parameters:
+    ///   - font: 字体
+    ///   - size: 大小
+    ///   - mode: 文字展示类型
+    /// - Returns: 文字大小
+    class func calculateSizeForText(textString text: String, systemFont font: UIFont, constrainedToSize size: CGSize, lineBreakMode mode: NSLineBreakMode) -> CGSize {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineBreakMode = mode
         paragraphStyle.alignment = .left
@@ -36,21 +36,21 @@ class LSPUtils: NSObject {
         attributes[NSAttributedString.Key.paragraphStyle] = paragraphStyle
         let rect = text.boundingRect(with: size, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: attributes, context: nil)
         return CGSize(width: ceil(rect.size.width), height: ceil(rect.size.height))
-     }
-     /// 计算文字宽度
-     /// - Parameters:
-     ///   - font: 字体
-     ///   - size: 大小
-     /// - Returns: 文字宽度
-     public func calculateWidthForText(textString text: String, font: UIFont, size: CGSize) -> CGFloat {
+    }
+    /// 计算文字宽度
+    /// - Parameters:
+    ///   - font: 字体
+    ///   - size: 大小
+    /// - Returns: 文字宽度
+    class func calculateWidthForText(textString text: String, font: UIFont, size: CGSize) -> CGFloat {
         return self.calculateSizeForText(textString: text, systemFont: font, constrainedToSize: size, lineBreakMode: NSLineBreakMode.byWordWrapping).width
-     }
-     /// 计算文字标签宽度
-     /// - Parameters:
-     ///   - label: 文字标签
-     ///   - height: 标签高度
-     /// - Returns: 标签宽度
-     public func calculateWidthForLabel(label: UILabel, height: CGFloat) -> CGFloat {
+    }
+    /// 计算文字标签宽度
+    /// - Parameters:
+    ///   - label: 文字标签
+    ///   - height: 标签高度
+    /// - Returns: 标签宽度
+    class func calculateWidthForLabel(label: UILabel, height: CGFloat) -> CGFloat {
         label.numberOfLines = 0
         label.lineBreakMode = NSLineBreakMode.byWordWrapping
         let autoWidth: CGFloat! = label.text?.boundingRect(with: CGSize(width: 1000, height: height), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : label.font!], context: nil).width
@@ -61,19 +61,58 @@ class LSPUtils: NSObject {
      ///   - font: 字体
      ///   - size: 大小
      /// - Returns: 文字高度
-     public func calculateHeightForText(textString text: String, font: UIFont, size: CGSize) -> CGFloat {
+    class func calculateHeightForText(textString text: String, font: UIFont, size: CGSize) -> CGFloat {
         return self.calculateSizeForText(textString: text, systemFont: font, constrainedToSize: size, lineBreakMode: NSLineBreakMode.byWordWrapping).height
-     }
-     /// 计算文字标签高度
-     /// - Parameters:
-     ///   - label: 文字标签
-     ///   - width: 标签宽度
-     /// - Returns: 标签高度
-     public func calculateHeightForLabel(label: UILabel, width: CGFloat) -> CGFloat {
+    }
+    /// 计算文字标签高度
+    /// - Parameters:
+    ///   - label: 文字标签
+    ///   - width: 标签宽度
+    /// - Returns: 标签高度
+    class func calculateHeightForLabel(label: UILabel, width: CGFloat) -> CGFloat {
         label.numberOfLines = 0
         label.lineBreakMode = NSLineBreakMode.byWordWrapping
         return (label.text?.boundingRect(with: CGSize(width: width, height: 1000), options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : label.font!], context: nil).height)!
-     }
+    }
+    
+    /// 字典转JSON
+    class func getJSONStringFromDictionary(dictionary:NSDictionary) -> String {
+        if (!JSONSerialization.isValidJSONObject(dictionary)) {
+            print("无法解析出JSONString")
+            return ""
+        }
+        let data : NSData! = try? JSONSerialization.data(withJSONObject: dictionary, options: []) as NSData
+        let JSONString = NSString(data:data as Data,encoding: String.Encoding.utf8.rawValue)
+        return JSONString! as String
+    }
+    
+    /// JSON转字典
+    class func getDictionaryFromJSONString(jsonString:String) ->NSDictionary{
+        let jsonData:Data = jsonString.data(using: .utf8)!
+        let dict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
+        if dict != nil {
+            return dict as! NSDictionary
+        }
+        return NSDictionary()
+    }
+    
+    /// base64加密
+    class func encodingBase64(String str:String) -> String{
+        let data = str.data(using: String.Encoding.utf8)
+        return (data?.base64EncodedString(options: .lineLength64Characters))!
+    }
+    
+    /// 颜色生成图片
+    class func createImageFrom(Color color:UIColor) -> UIImage{
+        let rect = CGRect(x:0, y:0, width:1, height:1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context:CGContext = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let image:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
     
     enum LSPPermissionType: Int {
         /// 相机
